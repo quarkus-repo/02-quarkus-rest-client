d4d7ce700af0aba92886fa594ec23c91886abfa4
AngelTravieso <angeltraviesoc@gmail.com>
2024-10-30 16:14:43 -0400

    Hot Reload y como leer propiedades definidas en el fichero .properties

--- src/main/java/org/at/ExtensionInfoApi.java
+++ src/main/java/org/at/ExtensionInfoApi.java
@@ -5,8 +5,11 @@ import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,6 +21,10 @@ public class ExtensionInfoApi {
     // Inyectar la clase extensionInfo
     @Inject
     ExtensionInfo extensionInfo;
+
+    // Leer valor del application.properties
+    @ConfigProperty(name = "greeting")
+    private String greeting;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON) // para devolver JSON
@@ -33,4 +40,12 @@ public class ExtensionInfoApi {
         }).collect(Collectors.toSet());
         return collect;
     }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("custom/{name}")
+    public String customHello(@PathParam("name") String name) {
+        return greeting + name + " how are yo doing?";
+    }
+
 }
